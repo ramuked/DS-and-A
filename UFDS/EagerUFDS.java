f1b6debaa5b82dd2680b850d0a70b36926db56be
ramuked <sushantkumar2025@gmail.com>
2022-05-29 15:00:50 +0530

    Added code for Lazy and Eager UFDS

---
+++ UFDS/EagerUFDS.java
@@ -0,0 +1,47 @@
+public class EagerUFDS{
+
+	private int[] id;
+	private int[] size;
+	
+	public EagerUFDS(int N){
+		
+		id = new int[N];
+		size = new int[N];
+		
+		for(int i = 0; i < N; i++){
+			id[i] = i;
+		}
+	}
+	
+	private int root(int i){
+		while (i != id[i]){
+			id[i] = id[id[i]];
+			i = id[i];
+		}
+		return i;
+	}
+	
+	
+	public boolean isConnnected(int p, int q){
+		return root(p) == root(q);
+			
+	}
+		
+	
+	public void union(int p, int q){
+		
+		int i = root(p);
+		int j = root(q);
+		if(size[i] < size[j]){
+			id[i] = id[j];
+			size[j] += size[i];
+		}
+		else{
+			id[j] = id[i];
+			size[i] += size[j];
+			
+			
+		}
+	}
+
+}
---
+++ UFDS/LazyUFDS.java
@@ -0,0 +1,33 @@
+public class LazyUFDS{
+
+	private int[] id;
+	
+	public LazyUFDS(int N){
+		
+		id = new int[N];
+		for(int i = 0; i < N; i++){
+			id[i] = i;
+		}
+	}
+	
+	private int root(int i){
+		while (i != id[i]) i = id[i];
+		return i;
+	}
+	
+	
+	public boolean isConnnected(int p, int q){
+		return root(p) == root(q);
+			
+	}
+		
+	
+	public void union(int p, int q){
+		
+		int i = root(p);
+		int j = root(q);
+		id[i] = j;
+			
+	}
+
+}
